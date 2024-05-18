5829116433a28c5ff5349bfb2ce7a97773825ed6
cylin1128 <cylin.ee12@nycu.edu.tw>
2024-05-18 08:18:26 +0000

    change global mem lateny to 80

--- Hardware/src/main/scala/lab11/Memory/DataMem.scala
+++ Hardware/src/main/scala/lab11/Memory/DataMem.scala
@@ -196,7 +196,7 @@ class DataMem(
     printf("\t\t======== Data Memory Dump ========\n")
     printf("\t\tFrom base address %d\n", baseAddr.U)
     // for (i <- 0 until 20) {
-    for (i <- 35 until 46) {
+    for (i <- 36 until 46) {
       val indexAddr = baseAddr + i * 4
       val data = memory(i)
       printf(
