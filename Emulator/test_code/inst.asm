05a0e4cd1b63e6e73f2dd1f2169234e65df65194
cylin1128 <cylin.ee12@nycu.edu.tw>
2024-06-17 17:57:45 +0800

    run binary search

--- Emulator/test_code/inst.asm
+++ Emulator/test_code/inst.asm
@@ -1,13 +1,62 @@
-lui x1, 0x00000008
-lw x2, 0(x1)
-addi x2, x2, 0x00000058
-sw x2, 4(x1)
-sb x2, 9(x1)
-lui x3, 0x00000010
-lui x3, 0x00000010
-lui x3, 0x00000010
-lui x3, 0x00000010
-lui x3, 0x00000010
+addi t0, zero, 0
+lui x06, 0x00000008
+addi x06, x06, 0x0000003c
+lw t1, 0(t1)
+addi t1, t1, -1
+add s10, t1, zero
+addi a5, zero, -1
+lui x18, 0x00000008
+addi x18, x18, 0x00000040
+lw s2, 0(s2)
+lui x08, 0x00000008
+addi x08, x08, 0x00000010
+jal ra, binary_search
+j x0, exit
+add a2, t0, t1
+srli a2, a2, 1
+blt s10, a2, fail
+add t2, a2, zero
+slli t2, t2, 2
+add t2, t2, s0
+lw t2, 0(t2)
+beq t2, s2, find
+beq t0, t1, fail
+blt s2, t2, less
+addi t0, a2, 1
+j x0, binary_search
+addi t1, a2, -1
+j x0, binary_search
+jalr x0, x1, x0
+addi a5, zero, -1
+lui x11, 0x00000008
+addi x11, x11, 0x00000008
+lui x10, 0x00000000
+addi x10, x10, 0x00000004
+addi a1, s2
+lui x10, 0x00000000
+addi x10, x10, 0x00000001
+lui x11, 0x00000008
+addi x11, x11, 0x0000000c
+lui x10, 0x00000000
+addi x10, x10, 0x00000004
+j x0, exit
+add a5, a2, zero
+lui x11, 0x00000008
+addi x11, x11, 0x00000000
+lui x10, 0x00000000
+addi x10, x10, 0x00000004
+addi a1, s2
+lui x10, 0x00000000
+addi x10, x10, 0x00000001
+lui x11, 0x00000008
+addi x11, x11, 0x00000004
+lui x10, 0x00000000
+addi x10, x10, 0x00000004
+addi a1, a5
+lui x10, 0x00000000
+addi x10, x10, 0x00000001
+j x0, exit
+hcf
 hcf
 nop
 nop
--- Hardware/src/main/resource/inst.asm
+++ Hardware/src/main/resource/inst.asm
@@ -1,75 +1,62 @@
-lui x02, 0x00000010
-addi x02, x02, 0x00000000
-lui x12, 0x00000000
-addi x12, x12, 0x00000008
-lui x13, 0x00000000
-addi x13, x13, 0x00000003
-lui x14, 0x00000000
-addi x14, x14, 0x00000006
-lui x15, 0x00000008
-addi x15, x15, 0x00000000
-lui x16, 0x00000008
-addi x16, x16, 0x00000080
-lui x17, 0x00000008
-addi x17, x17, 0x00000092
-addi sp, sp, -4
-sw ra, 0(sp)
-jal ra, conv2d
-addi a5, a5, 64
-addi a6, a6, 9
-jal ra, conv2d
-lw ra, 0(sp)
-addi sp, sp, 4
-lw s6, 0(a7)
-lw s7, 6(a7)
-lw s8, 12(a7)
-lw s9, 18(a7)
-lw s10, 24(a7)
-lw s11, 30(a7)
-hcf
-srli s0, a3, 1
-lui x05, 0x00000000
-addi x05, x05, 0x00000000
-bge t0, a4, endloop1
-lui x06, 0x00000000
-addi x06, x06, 0x00000000
-bge t1, a4, endloop2
-lui x07, 0x00000000
-addi x07, x07, 0x00000000
-bge t2, a3, endloop3
-lui x28, 0x00000000
-addi x28, x28, 0x00000000
-bge t3, a3, endloop4
-sub s1, t2, s0
-add s1, s1, t0
-addi s1, s1, 1
-sub s2, t3, s0
-add s2, s2, t1
-addi s2, s2, 1
-mul s3, s1, a2
-add s3, s3, s2
-add s3, s3, a5
-lb s3, 0(s3)
-mul s4, t2, a3
-add s4, s4, t3
-add s4, s4, a6
-lb s4, 0(s4)
-mul s3, s3, s4
-mul s4, t0, a4
-add s4, s4, t1
-add s4, s4, a7
-lb s5, 0(s4)
-add s5, s5, s3
-sb s5, 0(s4)
-addi t3, t3, 1
-blt t3, a3, loop4
-addi t2, t2, 1
-blt t2, a3, loop3
-addi t1, t1, 1
-blt t1, a4, loop2
-addi t0, t0, 1
-blt t0, a4, loop1
+addi t0, zero, 0
+lui x06, 0x00000008
+addi x06, x06, 0x0000003c
+lw t1, 0(t1)
+addi t1, t1, -1
+add s10, t1, zero
+addi a5, zero, -1
+lui x18, 0x00000008
+addi x18, x18, 0x00000040
+lw s2, 0(s2)
+lui x08, 0x00000008
+addi x08, x08, 0x00000010
+jal ra, binary_search
+j x0, exit
+add a2, t0, t1
+srli a2, a2, 1
+blt s10, a2, fail
+add t2, a2, zero
+slli t2, t2, 2
+add t2, t2, s0
+lw t2, 0(t2)
+beq t2, s2, find
+beq t0, t1, fail
+blt s2, t2, less
+addi t0, a2, 1
+j x0, binary_search
+addi t1, a2, -1
+j x0, binary_search
 jalr x0, x1, x0
+addi a5, zero, -1
+lui x11, 0x00000008
+addi x11, x11, 0x00000008
+lui x10, 0x00000000
+addi x10, x10, 0x00000004
+addi a1, s2
+lui x10, 0x00000000
+addi x10, x10, 0x00000001
+lui x11, 0x00000008
+addi x11, x11, 0x0000000c
+lui x10, 0x00000000
+addi x10, x10, 0x00000004
+j x0, exit
+add a5, a2, zero
+lui x11, 0x00000008
+addi x11, x11, 0x00000000
+lui x10, 0x00000000
+addi x10, x10, 0x00000004
+addi a1, s2
+lui x10, 0x00000000
+addi x10, x10, 0x00000001
+lui x11, 0x00000008
+addi x11, x11, 0x00000004
+lui x10, 0x00000000
+addi x10, x10, 0x00000004
+addi a1, a5
+lui x10, 0x00000000
+addi x10, x10, 0x00000001
+j x0, exit
+hcf
 hcf
 nop
 nop
